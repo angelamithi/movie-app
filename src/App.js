b77f9be29dc7ef755c727ddc50bf6a57e8caafea
+ <+>
2023-11-04 17:51:05 +0300

    add movie functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import MoviesPage from './MoviesPage';
 import Sidebar from './Sidebar';
 import Search from './Search';
-import Form from './Form';
+import AddMovie from './AddMovie';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
   const [dataMovies, setDataMovies] = useState([]);
+  const [showAddMovieForm, setShowAddMovieForm] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:3000/movies')
@@ -18,8 +19,24 @@ function App() {
       });
   }, []);
 
+  function handleAddMovie(newMovie) {
+    fetch('http://localhost:3000/movies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMovie),
+    })
+      .then((resp) => resp.json())
+      .then((movie) => {
+        setDataMovies([...dataMovies, movie]);
+        setShowAddMovieForm(false);
+      });
+  }
+
   return (
     <div className="ui grid">
+      <div className="ui grid">
       <div className='color-header'>
        <div className="ui center aligned segment">
         <div className="column">
@@ -27,6 +44,11 @@ function App() {
        </div>
       </div>
        </div>
+      {showAddMovieForm ? (
+        <AddMovie onFormSubmit={handleAddMovie} onCancel={() => setShowAddMovieForm(false)} />
+      ) : (
+        <button onClick={() => setShowAddMovieForm(true)}>Add a movie</button>
+      )}
       <div className='custom-search-bar'>
         
         <Search/>
@@ -48,6 +70,7 @@ function App() {
         </div>
         </div>
        </div>
+    </div>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,78 @@
-import React from "react";
-function Form (){
-    return(
-    <div>
-        <form className="form">
-            <input type="text" id="title" placeholder="Movie Title" value=""/> 
-            <input type="text" id="director" placeholder="Movie Director" value=""/> 
-            <label> Release Year:<input type="date" id="release-year" placeholder="Release Year" value=""/> </label>
-           
-            <input type="file" id="add-image" accept="image/*"/>
-            <input type="text" id="ratings" placeholder="Movie Ratings" value=""/> 
-           
-            <input type="textarea" id="comments" placeholder="Movie Comments" value=""/> 
-            <div>
-            <button type="submit">Add Movie</button>
-            </div>
-          
-        </form>
+import React, { useEffect, useState } from 'react';
+import MoviesPage from './MoviesPage';
+import Sidebar from './Sidebar';
+import Search from './Search';
+import AddMovie from './AddMovie';
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
 
+function App() {
+  const [dataMovies, setDataMovies] = useState([]);
+  const [showAddMovieForm, setShowAddMovieForm] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/movies')
+      .then((resp) => resp.json())
+      .then((movies) => {
+        setDataMovies(movies);
+        console.log(movies);
+      });
+  }, []);
+
+  function handleAddMovie(newMovie) {
+    fetch('http://localhost:3000/movies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMovie),
+    })
+      .then((resp) => resp.json())
+      .then((movie) => {
+        setDataMovies([...dataMovies, movie]);
+        setShowAddMovieForm(false);
+      });
+  }
+
+  return (
+    <div className="ui grid">
+      <div className="ui grid">
+      <div className='color-header'>
+       <div className="ui center aligned segment">
+        <div className="column">
+            <h1 className="ui header">Movie App</h1>
+       </div>
+      </div>
+       </div>
+      {showAddMovieForm ? (
+        <AddMovie onFormSubmit={handleAddMovie} onCancel={() => setShowAddMovieForm(false)} />
+      ) : (
+        <button onClick={() => setShowAddMovieForm(true)}>Add a movie</button>
+      )}
+      <div className='custom-search-bar'>
+        
+        <Search/>
+
+      </div>
+      
+
+      <div className='container'>
+       <div className='three wide column'>
+         <div className='sidebar'>
+          <Sidebar/>
+         </div>
+        </div>
+    
+      <div className="twelve wide column custom-background">
+       
+        <div className="space-cards">
+          <MoviesPage dataMovies={dataMovies} />
+        </div>
+        </div>
+       </div>
     </div>
-    )
+    </div>
+  );
 }
-export default Form;
+
+export default App;
